d3ba7524e6fb420c179fd613ddd4efbc0c19a045
divine <divinelubiku6@gmail.com>
2026-09-23 05:17:39 +0200

    feat : Silver lidst addition to enable scrolling

--- lib/screens/contact_groups.dart
+++ lib/screens/contact_groups.dart
@@ -1,13 +1,53 @@
 import 'package:flutter/cupertino.dart';
+import '../data/contact_group.dart';
+import '../main.dart';
 
 class ContactGroupsPage extends StatelessWidget {
   const ContactGroupsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoPageScaffold(
+       return _ContactGroupsView(  //returning the widet will the silver
+      selectedListId: 0,
+      onListSelected: (list) {
+        debugPrint(list.toString());
+      },
+    );
+  }
+}
+
+class _ContactGroupsView extends StatelessWidget {
+  const _ContactGroupsView({required this.onListSelected, this.selectedListId});
+
+  final int? selectedListId;
+  final void Function(ContactGroup) onListSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
       backgroundColor: CupertinoColors.extraLightBackgroundGray,
-      child: Center(child: Text('Contact Groups will go here')),
+      child: CustomScrollView(
+        slivers: [
+          const CupertinoSliverNavigationBar(largeTitle: Text('Lists')), //scrollable lists
+          SliverFillRemaining( //fills space in scroll -> non-silver widget
+            child: ValueListenableBuilder<List<ContactGroup>>(
+              valueListenable: contactGroupsModel.listsNotifier,
+              builder: (context, contactLists, child) {
+                return CupertinoListSection.insetGrouped(
+                  header: const Text('iPhone'),
+                  children: [
+                    for (final ContactGroup contactList in contactLists)
+                      CupertinoListTile(
+                        title: Text(contactList.label),
+                        onTap: () => onListSelected(contactList),
+                      ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
