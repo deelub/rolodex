3d3128995d23d648bc1b8a066088c9826881dd65
divine <divinelubiku6@gmail.com>
2026-09-23 06:16:47 +0200

    feat: detail view widget for large screen

--- lib/screens/adaptive_layout.dart
+++ lib/screens/adaptive_layout.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
-import 'package:rolodex/screens/contact_groups.dart';
 
+import 'contact_groups.dart';
 import 'contacts.dart';
 
 const largeScreenMinWidth = 600;
@@ -44,7 +44,13 @@ class _AdaptiveLayoutState extends State<AdaptiveLayout> {
       child: SafeArea(
         child: Row(
           children: [
-            const SizedBox(width: 320, child: Text('Sidebar placeholder')),
+            SizedBox(
+              width: 320,
+              child: ContactGroupsSidebar(
+                selectedListId: selectedListId,
+                onListSelected: _onContactListSelected,
+              ),
+            ),
             Container(width: 1, color: CupertinoColors.separator),
             const Expanded(child: Text('Details placeholder')),
           ],
